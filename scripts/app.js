1692bf554608d2fc30fdaf47b6b5a06570de6931
Shiloh Olotu <shiloholotu@gmail.com>
2025-02-10 07:08:02 -0600

    further styling

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,10 @@ let poemInd = 0;
 // pic, title, name, dates, blurb, theme connection, pdf 
 poems = 
 [
-    ["shakespeare", "Sonnet 116", "William Shakespeare", [1564,1616],
+    ["shakespeare", "Sonnet 116", "William Shakespeare (English Renaissance)", [1564,1616],
     "William Shakespeare lived during the English Renaissance, a period emphasizing the endurance of humanism, love, and the nature of truth. Similarly, \"Sonnet 116\" presents love as an unchanging, steadfast force that persists despite the speaker’s separation from his love interest. Shakespeare married a woman who was 8 years older than him (who he had already impregnated), suggesting he had a volatile view of marriage, reflected in the love interest’s death.",
     "In “Sonnet 116,” William Shakespeare presents the story of a speaker who’s love interest has been killed by divine forces and physically separated from him by a vast sea; and yet, his heart remains fully devoted to her. This work fits into the theme of “Love and Relationships” by portraying a romantic relationship that transcends death, suggesting that true love is a forever-enduring force.",
-    "AnnabelLeeTSWIFT.pdf"]
+    "AnnabelLeeTSWIFT"]
 ];
 
 function displayPoem(){
